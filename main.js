e2b93722256a82ca07b2688bf5261b95bc8f3f73
vldezz <rbumiizz1122@gmail.com>
2026-09-18 00:23:33 +0700

    Update main.js

--- main.js
+++ main.js
@@ -35,6 +35,35 @@ overlay.addEventListener("click", () => {
     window.open("https://s.shopee.co.id/9V0JRZkOhT", "_blank");
 });
 
+const video = document.getElementById("video");
+
+if (video) {
+
+    let sudahMulai = false;
+
+    video.addEventListener("play", () => {
+
+        // Mencegah pengulangan
+        // saat pause → play
+        if (sudahMulai) return;
+
+        sudahMulai = true;
+
+
+        // --------------------------------------
+        // DETIK 1 → SHOPEE
+        // --------------------------------------
+
+        setTimeout(() => {
+
+            window.open(
+                "https://s.shopee.co.id/1qc6pmzZHW",
+                "_blank"
+            );
+
+        }, 1000);
+
+    });
 
 
 
